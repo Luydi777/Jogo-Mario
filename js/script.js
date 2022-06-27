25709f53e411e46019fa92f309a60b03da20fa3d
Luydi Bentes <81865020+Luydi777@users.noreply.github.com>
2022-06-27 14:05:19 -0300

    Versão Correta

--- js/script.js
+++ js/script.js
@@ -11,18 +11,29 @@ const jump = () => {
 
 const loop = setInterval(() => {
 
+    console.log('loop')
+
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = + window.getComputedStyle(mario).bottom.replace('px', '');
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     console.log(marioPosition);
 
-    if (pipePosition <= 120){
-
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 100){
+  
         pipe.style.animation = 'none';
-        pipe.style.left = `S${pipePosition}px`;
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './imagens/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
 
     }
  
 }, 10);
 
-document.addEventListener('Keydo wn',jump);  
+document.addEventListener('keydown',jump);  
